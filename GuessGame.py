e8f21acb1e1e6de7c42291329b3f36464b78521b
Tomasz Wewior <kontowowag@gmail.com>
2024-06-09 22:30:14 +0200

    small change

--- GuessGame.py
+++ GuessGame.py
@@ -5,8 +5,8 @@ UPPER_BORDER_NUMBER = 30
 in_scale_text = " and not in the scale"
 
 print("Tell me Your name. I want to know who I talk to.")
-name = input()
-print("Hi, " + name)
+user_name = input()
+print("Hi, " + user_name)
 
 searched_number = random.randint(1, UPPER_BORDER_NUMBER)
 print(f"Guess a number between 1 and {UPPER_BORDER_NUMBER}. I will help You a little")
